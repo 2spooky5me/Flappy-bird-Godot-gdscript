9f4356bfaf0bdcabdbce2b5c1af3a6147c97811e
2spooky5me <giancarlotedesco11@gmail.com>
2024-12-05 23:24:48 -0400

    Game's in a playable state, still need to implemente art features (Background, sound, sprites, fade-in & fade-out anim, etc.)

--- Scripts/bird.gd
+++ Scripts/bird.gd
@@ -3,16 +3,32 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var start = false
+
+
+func _ready():
+	Game_State.connect("game_over", _on_game_over)
 
 
 func _physics_process(delta: float) -> void:
 
-	velocity += get_gravity() * delta
+	if start:
+		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("ui_up"):
-		velocity.y = JUMP_VELOCITY
+		if Input.is_action_just_pressed("ui_up"):
+			velocity.y = JUMP_VELOCITY
 
 	var collision = move_and_collide(velocity * delta)
 
 	if collision:
-		get_tree().quit()
+		Game_State.emit_signal("game_over")
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_up") and start == false:
+		Game_State.emit_signal("game_started")
+		start = true
+
+
+func _on_game_over() -> void:
+	self.queue_free()
--- Scripts/obstacle.gd
+++ Scripts/obstacle.gd
@@ -5,13 +5,18 @@ const SPEED = -400.0
 const JUMP_VELOCITY = -400.0
 
 
+
+
 func _physics_process(delta: float) -> void:
 	velocity.x = SPEED
 	var collision = move_and_collide(velocity * delta)
-	
-	if collision:
-		get_tree().quit()
 
+	if collision:
+		Game_State.emit_signal("game_over")
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	self.queue_free()
+
+
+func _on_area_2d_body_exited(_body:Node2D) -> void:
+	Game_State.emit_signal("point_scored")
